9a1ec68d176b177624462466ff29ea4866f240ab
Anshpreet Singh <anshpreetsingh3232@gmail.com>
2025-09-15 19:45:36 +0530

    Update Contact.tsx for resume

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -122,7 +122,7 @@ export const Contact = () => {
                   label: 'Resume',
                   hoverText: '❯❯❯❯',
                   type: 'link',
-                  target: 'https://drive.google.com/file/d/1HdLBFkIEuRbs3en91NeNYP4sZMz6HDGg/view?usp=sharing',
+                  target: 'https://drive.google.com/file/d/1uvgRHpsraqyvCDaNByO4lHWTIbzkvRtI/view?usp=sharing',
                   icon: <FileText size={20} />
                 }
               ]}
